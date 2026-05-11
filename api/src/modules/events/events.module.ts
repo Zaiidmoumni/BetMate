cdf798e48fa6504f221c124030b35cf1ba03209c
zaiidmo <vlphadev@gmail.com>
2026-05-11 11:06:52 +0100

    chore: clear api from index before history import

--- api/src/modules/events/events.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventsController } from './events.controller';
-import { OddsApiModule } from '../odds-api/odds-api.module';
-
-@Module({
-  imports: [OddsApiModule],
-  controllers: [EventsController],
-})
-export class EventsModule {}
